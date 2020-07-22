6ad444eda6d471dbd006078c501c1188ed1da936
Zach Grafton <zachgrafton@gmail.com>
2020-07-21 20:33:26 -0700

    mostly functional

--- dom.js
+++ dom.js
@@ -1,7 +1,26 @@
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+// Create a new list item when hitting "Enter"
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("input-item").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue !== '') {
+      document.getElementById("ul-list-item").appendChild(li);
     }
-  }
+    document.getElementById("input-item").value = "";
+}
+
+
+document.getElementById("input-item")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        newElement();
+    }
+});
+
+// checkbox
+
+  // markkDone()
+
+  // deleteItem()
