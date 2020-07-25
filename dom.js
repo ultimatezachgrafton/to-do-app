3dd251f60530da27f7ee99a5b390d7e671cf0cf1
Zach Grafton <zachgrafton@gmail.com>
2020-07-24 21:59:33 -0700

    functional - all but storage

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
+document.getElementById("input-item").addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+      newElement();
+  }
+});
+
 // Create a new list item when hitting "Enter"
 function newElement() {
     let li = document.createElement("li");
@@ -6,21 +13,16 @@ function newElement() {
     li.appendChild(t);
     if (inputValue !== '') {
       document.getElementById("ul-list-item").appendChild(li);
-    }
+    }  
     document.getElementById("input-item").value = "";
 }
 
+document.getElementById("ul-list-item").onclick = function(e) {
+  if (e.target.style.textDecoration == "line-through") {
+    e.target.parentElement.removeChild(e.target);
+  } else {
+    e.target.style.textDecoration="line-through";
+  }
+}
 
-document.getElementById("input-item")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        newElement();
-    }
-});
-
-// checkbox
-
-  // markkDone()
-
-  // deleteItem()
+  // items saved to local storage
